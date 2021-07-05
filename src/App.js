6102b825661478784becdd8954de3dc7bffe7ca9
Michael Agung <michaelputra.agung@gmail.com>
2021-07-05 14:07:39 +0700

    Adding Links & Router in LoginButton

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState,useEffect} from "react";
-import LoginButton from './components/LoginButton'
 import {Link, BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Auth from "./components/Auth/Auth"
 import Button from 'react-bootstrap/Button';
-import GoogleLogin from 'react-google-login';
+import CiteButton from './components/CitationGenerator/CiteButton'
 const {OAuth2Client} = require('google-auth-library');
 
 function App() {
-  const [user, setUser] = useState({});
-  const onSuccess = (res) => {
-    setUser({name:res.Ys.Ve})
-    console.log(res.Ys.Ve)
-    console.log("datatype:",typeof res.Ys.Ve)
-    console.log('Success! currentUser:', user);
-  }
-  const onFailure = (res) =>{
-    console.log("login failed! res:",res);
-  }
 
-  useEffect(() => {
-    debugger
-    if(user.name !== undefined)
-    {
-      debugger
-      console.log(user)
-    }
-  },[user])
 
   return (
     <div className="App">
@@ -40,39 +20,20 @@ function App() {
           <Link to="/">
             <Button>Generate Citation!</Button>
           </Link>
-          <Link to="/auth">
-            Auth
-          </Link>
           <Link to={{pathname:"http://www.google.com"}}>
             <Button>Google!</Button>
           </Link>
           </nav>
+
+
         <Switch>
           <Route exact path="/">
-            {user && (
-              <h1>Welcome, {user.Bd}</h1>
-            )}
-            <GoogleLogin
-              clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-              buttonText="Log in with Google"
-              onSuccess={onSuccess}
-              onFailure={onFailure}
-              cookiePolicy={'single_host_origin'}
-            />
-            {/*}<LoginButton/>*/}
+            <CiteButton />
           </Route>
           <Route path="/auth"> <Auth/> </Route>
         </Switch>
         </div>
         </Router>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
