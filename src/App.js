f4c7682a7e4f9448fa66da4f1639c5d622492961
Michael Agung <michaelputra.agung@gmail.com>
2021-07-05 15:05:07 +0700

    further coding sample code to run locally

--- src/App.js
+++ src/App.js
@@ -6,13 +6,51 @@ import Button from 'react-bootstrap/Button';
 import CiteButton from './components/CitationGenerator/CiteButton'
 const {OAuth2Client} = require('google-auth-library');
 
-function App() {
+//web3component
+import React, {useState} from "react";
+import Web3 from "web3";
+import {simpleStorageAbi} from "./abi/abis";
+const web3 = new Web3(Web3.givenProvider);
+const contractAddr = '0x72c6Dd09F9fC623fab138486Bea6c0ebeD78e4f5';
+const SimpleContract = new web3.eth.Contract(simpleStorageAbi, contractAddr);
+//end web3component
 
+function App() {
+  //web3component
+  const [number, setNumber] = useState(0);
+  const [getNumber, setGetNumber] = useState('0x00');
+  const handleGet = async (e) => {
+    e.preventDefault();
+    const result = await SimpleContract.methods.get().call();
+    setGetNumber(result);
+    console.log(result);
+  }
+  //end web3component
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        /*<img src={logo} className="App-logo" alt="logo" />*/
+        //web3component
+        <form onSubmit={handleSet}>
+          <label>
+            Set Number:
+            <input
+              type="text"
+              name="name"
+              value={number}
+              onChange={ e => setNumber(e.target.value) } />
+          </label>
+          <input type="submit" value="Set Number" />
+        </form>
+        <br/>
+        <button
+          onClick={handleGet}
+          type="button" >
+          Get Number
+        </button>
+        { getNumber }
+        //end web3component
 
         <Router>
         <div>
